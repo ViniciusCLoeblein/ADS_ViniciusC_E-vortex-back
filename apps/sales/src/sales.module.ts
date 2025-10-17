b746f3fc0acea4f8a3cc991e4cef4d9b31d25a69
ViniciusCLoeblein <391411@grazziotin.com.br>
2025-10-17 13:40:43 -0300

    fix sales module

--- apps/sales/src/sales.module.ts
+++ apps/sales/src/sales.module.ts
@@ -5,15 +5,12 @@ import { PostgresDatabaseService } from 'apps/generics/database';
 import { SalesControllerRMQ } from './sales.controller.rmq';
 import { SalesService } from './sales.service';
 import { SalesRepository } from './sales.repository';
-import { APP_GUARD } from '@nestjs/core';
-import { JwtAuthGuard } from 'apps/generics/guards/jwt-auth.guard';
 import { CarrinhosEntity } from 'apps/entities/carrinhos.entity';
 import { ItensCarrinhoEntity } from 'apps/entities/itens_carrinho.entity';
 import { ProdutosEntity } from 'apps/entities/produtos.entity';
 import { VariacoesProdutoEntity } from 'apps/entities/variacoes_produto.entity';
 import { ImagensProdutoEntity } from 'apps/entities/imagens_produto.entity';
 import { FavoritosEntity } from 'apps/entities/favoritos.entity';
-import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -36,31 +33,9 @@ import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
       ImagensProdutoEntity,
       FavoritosEntity,
     ]),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async () => {
-        const options: JwtModuleOptions = {
-          secret: process.env.JWT_PRIVATE_KEY,
-          signOptions: {
-            expiresIn: process.env.JWT_EXPIRES_IN,
-            algorithm: 'HS256',
-          },
-        };
-
-        return options;
-      },
-      inject: [ConfigService],
-    }),
   ],
   controllers: [SalesControllerRMQ],
-  providers: [
-    SalesRepository,
-    SalesService,
-    {
-      provide: APP_GUARD,
-      useClass: JwtAuthGuard,
-    },
-  ],
+  providers: [SalesRepository, SalesService],
   exports: [SalesService],
 })
 export class SalesModule {}
